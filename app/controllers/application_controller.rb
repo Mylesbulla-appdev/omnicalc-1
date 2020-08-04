7444a8704d61a3ca00ada8a895444eae59eae08d
hydropero <mylesjbulla@gmail.com>
2020-08-04 16:03:06 +0000

    squaring part working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb" })
+  end
+
+  def calculate_square
+    @num = params.fetch('square_this_num').to_f
+    @square_of_num = @num * @num
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
+
 
 end
